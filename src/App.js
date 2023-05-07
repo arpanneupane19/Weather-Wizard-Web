3b8955b44c09e84507e1fc619737024793ea85e0
Arpan Neupane <arpanneupane19@gmail.com>
2023-05-07 18:59:57 -0500

    updated some directories and removed unnecessary code

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import React from "react";
 import Weather from "./components/Weather";
 import Recents from "./components/Recents";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import dotenv from "dotenv";
-import HttpsRedirect from "react-https-redirect";
 
 function App() {
   return (
-    <HttpsRedirect>
-      <Router>
-        <Switch>
-          <Route exact path={["/", "/home"]}>
-            <Weather />
-          </Route>
-        </Switch>
-        <Switch>
-          <Route exact path="/recents">
-            <Recents />
-          </Route>
-        </Switch>
-      </Router>
-    </HttpsRedirect>
+    <Router>
+      <Switch>
+        <Route exact path={["/", "/home"]}>
+          <Weather />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route exact path="/recents">
+          <Recents />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
